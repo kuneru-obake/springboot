dfb865768a90bbf4bd87c25535876bcdb62958aa
kuneru-obake <kuneru.obake@gmail.com>
2017-02-06 23:05:33 +0900

    test

---
+++ example-spring-ws-simple/src/main/java/sogdice/example/spring/boot/websocket/simple/Application.java
@@ -0,0 +1,21 @@
+package sogdice.example.spring.boot.websocket.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@Controller
+public class Application {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
